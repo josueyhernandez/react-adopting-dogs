3d9f6b1c18cde99714cd1542e04923befebab8c4
Josue Hernandez <josueyhernandez18@gmail.com>
2021-05-24 13:03:38 -0400

    Dog Images shows, countDown works, and Adoption Images works

--- App.js
+++ App.js
@@ -1,19 +1,94 @@
-const {useState, useEffect} = React;
+const {useState, useEffect, useRef} = React;
+const url = "https://dog.ceo/api/breeds/image/random"
 
+//Madalyn help me with lines 7-9, 32, and 70. 
 function App() {
-  // Your Code Here
+  const [dogImages, setDogImages] = React.useState("")
+  const [time, setTime] = React.useState(5)
+  const [adoptedDogs, setAdoptedDogs] = React.useState([])
+  const stateRef = React.useRef()
+  stateRef.current = dogImages
+
+  function fetchDog() {
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => setDogImages([data.message]))
+    setTime(5)
+  }
+
+  
+  React.useEffect(() => {
+    fetchDog()
+    changeImage
+    console.log("useEffect Works!")
+    window.addEventListener("keydown", changeImage)
+    return () => {
+      window.removeEventListener("keydown", changeImage)
+    }
+  }, [])
+
+  React.useEffect(() => {
+    if (time < 0) {
+      return fetchDog()
+    }
+    let timer = setInterval(() => setTime(time - 1), 1000)
+    return () => clearInterval(timer)
+  })
+ 
+  function changeImage(event) {
+    if (event.code === "ArrowRight") {
+      console.log("You press right arrow key")
+      setAdoptedDogs((prevState) => [...prevState, stateRef.current])
+      fetchDog()
+    } else if (event.code === "ArrowLeft") {
+      console.log("You press left arrow key")
+      fetchDog()
+    } else {
+      console.log("Not Left or Right arrow key")
+    }
+  }
+  
+  // function countDown() {
+  //   let timeLeft = 5
+  //   setInterval(() => {
+  //     if (timeLeft === 0) {
+  //       fetchDog()
+  //       clearInterval(timeLeft = 6)
+  //     }
+  //     let timeText = document.getElementById("time")
+  //     timeText.innerHTML = timeLeft -= 1
+  //     console.log(timeLeft)
+  //   }, 1000)
+  // }
 
   return (
     <div className="app">
       <h1>Doggie Speed Dating</h1>
       <h3>Press the arrow keys on your keyboard. Left to skip, Right to Adopt.</h3>
       <section>
-        Your HTML Here!
+        <h3>Current Dog:</h3>
+        <img 
+        width={"200px"}
+        height={"200px"}
+        src={dogImages}
+        />
+        <h2>Time Remaining: <span id="time">{time}</span></h2>
+      </section>
+      <section>
+        <h3>Adopted dogs:</h3>
+        {adoptedDogs.map((dog, index) => (
+          <img
+           src={dog}
+           height={"200px"}
+           weight={"200px"}
+           key={index}
+           />
+
+        ))}
       </section>
     </div>
-  );
+  )
 }
-
 //Render the application
 ReactDOM.render(
   <App />,
